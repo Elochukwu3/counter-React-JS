5342c994d2dc6f4270b41dde62ba7dd421c2993b
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-02 18:43:50 +0100

    saved app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
-import './App.css';
-import Counter from './Counter'
+// import './App.css';
+// import Counter from './Counter'
 
-function App() {
-  return (
-   <div className = "App-header">
-   <div className='Holder'>
-   <h2>This is a counter</h2>
-    <Counter/>
-   </div>
-   </div>
-  );
-}
+// function App() {
+//   return (
+//    <div className = "App-header">
+//    <div className='Holder'>
+//    <h2>This is a counter</h2>
+//     <Counter/>
+//    </div>
+//    </div>
+//   );
+// }
 
-export default App;
+// export default App;
