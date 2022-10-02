c0f01d71367eb817290538cb89605f48fedc5c8a
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-02 18:51:42 +0100

    inline style saved

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,11 +5,6 @@ import './style.css'
 function Counter() {
     const [count, setCount] = useState(0);
    
-  //  let increase;
-  const increment =()=>{ 
-    setCount((prev)=> prev +1)
-// increase = "increase";
- }
 useEffect(()=>{
     setCount(0)
 }, [])
@@ -19,7 +14,7 @@ useEffect(()=>{
             <h1 className='App-count-content'>{count}</h1>
          <div className='Btn-holder'>
            <button onClick={()=> setCount((prev)=> (prev <= 0 ? prev = 0 : prev -1  ))} className= "App-button"> Decrese</button>
-           <button onClick={() =>increment() } className= "App-button"> Increase</button>
+           <button onClick={() =>setCount((prev)=> prev +1)} className= "App-button"> Increase</button>
           </div>
         </div>
        
